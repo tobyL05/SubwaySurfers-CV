e5cf6fc87db0d66cd2ecdd654286e77a0556fee6
tobyL05 <toby.lau075@gmail.com>
2024-05-02 22:20:13 -0700

    fixed code style

--- main.py
+++ main.py
@@ -1,66 +1,80 @@
+"""Main class"""
+
 from abc import abstractmethod
 from typing import Type, List
+import webbrowser as wb
 import tensorflow as tf
 import numpy as np
 import cv2
-# import tensorflow_hub as hub
-import webbrowser as wb
 import pyautogui
 
 class Observable:
-    def addObserver(self, obs: Type['Observer']):
+    """Represents an Abstract Observable class"""
+
+    def add_observer(self, obs: Type['Observer']):
+        """
+        Adds an observer to the list of observers 
+        """
         self.observers.append(obs)
 
-    def notifyObservers(self):
+    def notify_observers(self):
+        """
+        Updates each observer 
+        """
         for observer in self.observers:
             observer.update(self)
 
 class Observer:
-    def __init__(self):
-        pass
+    """Represents an Abstract Observer class"""
 
     @abstractmethod
     def update(self, obs: Observable):
+        """
+        Updates the observer based on Observable's state 
+        """
         pass
 
 class VideoStream(Observable):
+    """Handles Webcam feed and pose detection."""
     observers :List[Observer] = []
+
     def __init__(self):
+        """Constructor loads MoveNet model and starts webcam capture"""
         self.interpreter = tf.lite.Interpreter(model_path='model/singlepose-lightning.tflite')
         self.interpreter.allocate_tensors()
         # self.module = hub.load("https://tfhub.dev/google/movenet/singlepose/lightning/4")
         self.input_size = 192
         self.vid = cv2.VideoCapture(0)
+        self.pos = ""
+        self.state = ""
 
     def start(self):
-        self.addObserver(Game())
+        """Starts the pose detection"""
+        self.add_observer(Game())
         while self.vid.isOpened():
-            ret, frame = self.vid.read()
+            _, frame = self.vid.read()
             frame = cv2.flip(frame,1)
             h, w  = frame.shape[:2]
-            self.drawLines(frame, w, h)
+            self.draw_lines(frame, w, h)
 
             img = frame.copy()
             img = tf.image.resize_with_pad(np.expand_dims(img, axis=0), 192,192)
             input_image = tf.cast(img, dtype=tf.float32)
 
-            # # Setup input and output 
+            # Setup input and output
             input_details = self.interpreter.get_input_details()
             output_details = self.interpreter.get_output_details()
 
-            # # Make predictions 
+            # Make predictions 
             self.interpreter.set_tensor(input_details[0]['index'], np.array(input_image))
             self.interpreter.invoke()
             keypoints_with_scores = self.interpreter.get_tensor(output_details[0]['index'])[0, 0]
 
-            # Run model inference.
-            # keypoints_with_scores = self.movenet(input_image)
-
             orig_w, orig_h = frame.shape[:2]
-            M = self.get_affine_transform_to_fixed_sizes_with_padding((orig_w, orig_h), (192, 192))
+            Mat = self.get_affine_transform_to_fixed_sizes_with_padding((orig_w, orig_h), (192, 192))
             # M has shape 2x3 but we need square matrix when finding an inverse
-            M = np.vstack((M, [0, 0, 1]))
-            M_inv = np.linalg.inv(M)[:2]
+            Mat = np.vstack((Mat, [0, 0, 1]))
+            M_inv = np.linalg.inv(Mat)[:2]
             xy_keypoints = keypoints_with_scores[:, :2] * 192
             xy_keypoints = cv2.transform(np.array([xy_keypoints]), M_inv)[0]
             keypoints_with_scores = np.hstack((xy_keypoints, keypoints_with_scores[:, 2:]))
@@ -76,16 +90,23 @@ class VideoStream(Observable):
         cv2.destroyAllWindows()
 
     def get_affine_transform_to_fixed_sizes_with_padding(self,size, new_sizes):
+        """
+        Convert model output back to actual coordinates 
+        for accurate keypoint drawing.
+        """
         width, height = new_sizes
         scale = min(height / float(size[1]), width / float(size[0]))
-        M = np.float32([[scale, 0, 0], [0, scale, 0]])
-        M[0][2] = (width - scale * size[0]) / 2
-        M[1][2] = (height - scale * size[1]) / 2
-        return M
+        Mat = np.float32([[scale, 0, 0], [0, scale, 0]])
+        Mat[0][2] = (width - scale * size[0]) / 2
+        Mat[1][2] = (height - scale * size[1]) / 2
+        return Mat
 
-    def drawLines(self, frame, w, h):
+    def draw_lines(self, frame, w, h):
+        """
+        Draw border lines on webcam output 
+        """
         cv2.line(frame, (0,int(h/2 - 20)),(w,int(h/2 - 20)),(255,255,255),2) 
-        cv2.line(frame, (0,int(h/2 + 100)),(w,int(h/2 + 100)),(255,255,255),2) 
+        cv2.line(frame, (0,int(h/2 + 100)),(w,int(h/2 + 100)),(255,255,255),2)
         cv2.line(frame,(int(w/2 - 200),0),(int(w/2 - 200),h),(255,255,255),2)
         cv2.line(frame,(int(w/2 + 200),0),(int(w/2 + 200),h),(255,255,255),2)
 
@@ -112,8 +133,10 @@ class VideoStream(Observable):
         return keypoints_with_scores
 
     def handle_keypoints(self,frame, keypoints, confidence_threshold):
-        h, w, c = frame.shape
-        # shaped = np.squeeze(np.multiply(keypoints, [y,x,1]))[5:7] #take points up to shoulder point
+        """
+        Draw each keypoint and calculate the position 
+        """
+        h, w, _ = frame.shape
     
         sumx = 0
         sumy = 0
@@ -123,20 +146,16 @@ class VideoStream(Observable):
             sumy += y
             if conf > confidence_threshold:
                 cv2.circle(frame, (int(x), int(y)), 10, (0,0,255),-1)
-        avgx = sumx/2
-        avgy = sumy/2
+        x_mid = sumx/2
+        y_mid = sumy/2
 
-        cv2.circle(frame,(int(avgx),int(avgy)),20, (0,255,0),-1) # midpoint
-        self.calculate_pos(w, h, avgx,avgy)
+        cv2.circle(frame,(int(x_mid),int(y_mid)),20, (0,255,0),-1) # midpoint
+        self.calculate_pos(w, h, x_mid,y_mid)
     
     def calculate_pos(self, width, height, x, y): 
-        # cv2.line(frame, (0,int(h/2 - 20)),(w,int(h/2 - 20)),(255,255,255),2) jump
-        # cv2.line(frame, (0,int(h/2 + 100)),(w,int(h/2 + 100)),(255,255,255),2) slide
-        # cv2.line(frame,(int(w/2 - 100),0),(int(w/2 - 100),h),(255,255,255),2) left
-        # cv2.line(frame,(int(w/2 + 100),0),(int(w/2 + 100),h),(255,255,255),2) right
-
-        self.pos = ""
-        self.state = ""
+        """
+        Determines player's new position and state given current x,y position. 
+        """
         if x >= (width/2 + 200):
             self.pos = "right"
         elif x <= (width/2 - 200):
@@ -151,40 +170,54 @@ class VideoStream(Observable):
         else:
             self.state = "run"
         
-        self.notifyObservers()
+        self.notify_observers()
 
 class Game(Observer):
+    """Represents the Game itself"""
+
     def __init__(self):
+        """
+        Constructor opens the game in a new default browser window 
+        """
         super()
-        self.currentPos = "middle"
-        self.currentState = "run"
+        self.current_pos = "middle"
+        self.current_state = "run"
         wb.open("https://subwaysurfersgame.io/subway-surfers-game.embed", new=1)
 
     def update(self, obs:Observable):
-        prevPos = self.currentPos
-        if self.currentPos != obs.pos:
-            self.currentPos = obs.pos
-            self.updatePos(prevPos, self.currentPos)
+        """
+        Updates player's position and state
+        """
+        prev_pos = self.current_pos
+        if self.current_pos != obs.pos:
+            self.current_pos = obs.pos
+            self.update_pos(prev_pos, self.current_pos)
 
-        if self.currentState != obs.state:
-            self.currentState = obs.state
-            self.updateState(self.currentState)
+        if self.current_state != obs.state:
+            self.current_state = obs.state
+            self.update_state(self.current_state)
 
 
-    def updatePos(self, prevPos, newPos):
-        match newPos:
+    def update_pos(self, prev_pos, new_pos):
+        """
+        Updates player's position 
+        """
+        match new_pos:
             case "middle":
-                if prevPos == "left":
+                if prev_pos == "left":
                     pyautogui.press("right") 
-                elif prevPos == "right":
+                elif prev_pos == "right":
                     pyautogui.press("left")
             case "left":
                 pyautogui.press("left")
             case "right":
                 pyautogui.press("right")
 
-    def updateState(self, newState):
-        match newState:
+    def update_state(self, new_state):
+        """
+        Updates player's state 
+        """
+        match new_state:
             case "jump":
                 pyautogui.press("up")
             case "slide":
